6d3760d1bbb2049e524d4b07376885476c424493
dkhole <dkhlee419@gmail.com>
2020-08-21 01:37:58 +1000

    before changing title

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-let btn = document.querySelector("input");
+let btn = document.querySelector("#size");
+let choose = document.getElementById("choose");
+let random = document.getElementById("random");
+let eraser = document.getElementById("eraser");
 let container = document.getElementById("container");
+let dimensions = document.getElementById("dimensions");
 let rows = 16;
 let cols = 16;
 
@@ -12,7 +16,19 @@ btn.addEventListener("click", () => {
     }
 
     generateGrid(rows, cols);
-    colorCell();
+    colorChoice(choose.value);
+})
+
+choose.addEventListener("change", () => {
+    colorChoice(choose.value);
+})
+
+random.addEventListener("click", () => {
+    colorRandom();
+})
+
+eraser.addEventListener("click", () => {
+    colorChoice("#FFFFFF");
 })
 
 function generateGrid(r, c) {
@@ -25,9 +41,10 @@ function generateGrid(r, c) {
     let root = document.documentElement;
     root.style.setProperty("--grid-rows", r);
     root.style.setProperty("--grid-cols", c);
+    dimensions.textContent = r + " x " + c;
 }
 
-function colorCell() {
+function colorRandom() {
     let cells = document.querySelectorAll("div.cell");
     let r = 255;
     let g = 255;
@@ -38,12 +55,29 @@ function colorCell() {
             r = Math.floor(Math.random() * 256);
             g = Math.floor(Math.random() * 256);
             b = Math.floor(Math.random() * 256);
-            cell.style.backgroundColor = "rgb(" + r + "," + b + "," + g + ")";
+            cell.style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
         })
     })
 }
 
-generateGrid(17, 17);
+function hexToR(h) {return parseInt((cutHex(h)).substring(0,2),16)}
+function hexToG(h) {return parseInt((cutHex(h)).substring(2,4),16)}
+function hexToB(h) {return parseInt((cutHex(h)).substring(4,6),16)}
+function cutHex(h) {return (h.charAt(0)=="#") ? h.substring(1,7):h}
 
-colorCell();
+function colorChoice(usersChoice) {
+    let cells = document.querySelectorAll("div.cell");
+    let hex = cutHex(usersChoice);
+    cells.forEach((cell) => {
+        cell.addEventListener("mouseenter", () => {
+            let r = hexToR(hex);
+            let g = hexToG(hex);
+            let b = hexToB(hex);
+            cell.style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
+        })
+    })
+}
+
+generateGrid(rows, cols);
+colorChoice(choose.value);
 
